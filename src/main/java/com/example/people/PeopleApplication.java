5dc164506577084ad5e577177074a684fa89bbae
FrankS <FrankS@D10045.peoplestrust.com>
2018-03-28 10:12:27 -0400

    test war generation

--- src/main/java/com/example/people/PeopleApplication.java
+++ src/main/java/com/example/people/PeopleApplication.java
@@ -5,20 +5,28 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.example.people.model.Employee;
 import com.example.people.repository.EmployeeRepository;
 
 @SpringBootApplication
-public class PeopleApplication {
+public class PeopleApplication extends SpringBootServletInitializer{
 
 	private static final Logger logger = LoggerFactory.getLogger(PeopleApplication.class);
+
+    private static Class<PeopleApplication> applicationClass = PeopleApplication.class;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PeopleApplication.class, args);
 	}
 	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(applicationClass);
+	    }
 	
 	@Bean
 	public CommandLineRunner setup(EmployeeRepository employeeRepository) {
